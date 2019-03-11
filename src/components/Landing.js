fb6480849158f6622343a032556cfb4203c566d6
Prateek Karkare <prateek.karkare@gmail.com>
2019-03-11 18:16:20 +0530

    Landing page with navbar no scroll fonts bad

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,8 +7,11 @@ import withStyles from "@material-ui/core/styles/withStyles";
 // @material-ui/icons
 
 // core components
+import Header from "./Header/Header.js";
 import Parallax from "./Parallax.js";
-
+import GridContainer from "./Grid/GridContainer.js";
+import GridItem from "./Grid/GridItem.js";
+import Button from "./CustomButtons/Button.js";
 import landingPageStyle from "../assets/jss/views/landingPageStyle.js";
 
 // Sections for this page
@@ -23,7 +26,44 @@ class LandingPage extends React.Component {
     const { classes, ...rest } = this.props;
     return (
       <div>
-        <Parallax filter image={require("../assets/img/handshake.jpg")} />
+        <Header
+          color="transparent"
+          routes={dashboardRoutes}
+          brand="Material Kit React"
+          rightLinks={"Links"}
+          fixed
+          changeColorOnScroll={{
+            height: 400,
+            color: "white"
+          }}
+          {...rest}
+        />
+        <Parallax filter image={require("../assets/img/handshake.jpg")}>
+          <div className={classes.container}>
+            <GridContainer>
+              <GridItem xs={12} sm={12} md={6}>
+                <h1 className={classes.title}>Your Story Starts With Us.</h1>
+                <h4>
+                  Every landing page needs a small description after the big
+                  bold title, that's why we added this text here. Add here all
+                  the information that can make you or your product create the
+                  first impression.
+                </h4>
+                <br />
+                <Button
+                  color="danger"
+                  size="lg"
+                  href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fas fa-play" />
+                  Know More
+                </Button>
+              </GridItem>
+            </GridContainer>
+          </div>
+        </Parallax>
       </div>
     );
   }
